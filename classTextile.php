600fb862dd9bb0de536260cb8539ea257b232691
netcarver <nc.github@netcarving.com>
2012-11-06 12:34:37 +0000

    Extend regex comments

--- classTextile.php
+++ classTextile.php
@@ -1550,7 +1550,8 @@ class Textile
 		return preg_replace_callback('/
 			(?P<pre>\[)?                  # Optionally open with a square bracket eg. Look ["here":url]
 			"                             # literal " marks start of the link
-			(?P<inner>[^"]+?)             # capture the content of the inner "..." part of the link
+			(?P<inner>[^"]+?)             # capture the content of the inner "..." part of the link, can be anything except " but
+			                              # do not worry about matching class, id, lang or title yet
 			":                            # literal ": marks end of atts + text + title block
 			(?P<url>[\w\/\.]{1})          # first character following end of displayed chars
 			(?P<urlx>[^'.$stopchars.']*)  # url upto a stopchar
